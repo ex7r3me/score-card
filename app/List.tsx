166a5eabdbd12861e63f588d9c16e35b1d1116d3
Sia <sia@MBPsomtrSiavash.localdomain>
2023-09-06 16:22:37 +0200

    Add a text input and a list

---
+++ app/List.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React, { useState } from 'react';
+
+const List = () => {
+  const [items, setItems] = useState(Array<string>);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Names</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
