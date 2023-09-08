721cdee20410d279e801a51e87378a946a586988
Anders Silfverberg <anders.silfverberg@regent.se>
2023-09-08 11:48:24 +0200

    Added golf and yahtzee pages to app router

---
+++ app/yahtzee/page.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Page from './page'
+
+test('it displays heading', async () => {
+    render(<Page />)
+    await screen.findByRole('heading')
+    expect(screen.getByRole('heading')).toHaveTextContent('Yahtzee')
+})
---
+++ app/yahtzee/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1 role='heading' className="text-3xl font-bold underline">Yahtzee</h1>
+      </div>
+    );
+  }
+  
