6e0f6c9256897f2092be0ce0602600d8f64b91d7
Siavash Ahmadpour <ex7r3me@Siavashs-Mac-mini.local>
2023-09-08 11:51:41 +0200

    add color pallette to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        "primary-gray": "#2C302E",
+        "accent-gray": "#474A48",
+        "dark-green": "#62A87C",
+        "light-green": "#7EE081",
+        "beige": "#F1D6B8",
+        "white": "#FFFFFF"
+      }
     },
   },
   plugins: [],
