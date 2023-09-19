45b2c71607c6e06c762c3f1f80edc12439884916
Anders Silfverberg <anders.silfverberg@regent.se>
2023-09-19 09:11:23 +0200

    Added max width to golf card

--- app/golf/page.tsx
+++ app/golf/page.tsx
@@ -2,7 +2,7 @@ import GolfScorecard from "@/components/golf-scorecard/golf-scorecard";
 
 export default function Home() {
   return (
-    <div className="bg-beige p-4 m-4 rounded-2xl overflow-x-auto">
+    <div className="bg-beige p-4 m-4 ml-auto mr-auto max-w-md rounded-2xl overflow-x-auto">
       <h1 role='heading' className="text-3xl font-bold my-2 text-center">Golf</h1>
       <GolfScorecard />
     </div>
