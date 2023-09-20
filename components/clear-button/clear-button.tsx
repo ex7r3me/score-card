16a22c4d5916812e6d87a2e8361b7eb4e59e3815
Anders Silfverberg <anders.silfverberg@regent.se>
2023-09-20 15:05:00 +0200

    Changed buttons to react area Buttons

--- components/clear-button/clear-button.tsx
+++ components/clear-button/clear-button.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { TrashIcon } from '@heroicons/react/24/outline'
+import { Button } from 'react-aria-components'
 
 export default function VerificationDialog({ onAccept }: { onAccept: () => void }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -15,9 +16,9 @@ export default function VerificationDialog({ onAccept }: { onAccept: () => void 
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)} className='inline-block bg-accent-gray text-white py-1 px-3 font-bold rounded-md' aria-label='clear board'>
+      <Button onPress={() => setIsOpen(true)} className='inline-block bg-accent-gray text-white py-1 px-3 font-bold rounded-md' aria-label='clear board'>
         <TrashIcon aria-hidden="true" focusable='false' className='w-6 h-6 text-white' />
-      </button>
+      </Button>
       { isOpen && (<div className='bg-black/70 fixed z-20 top-0 left-0 w-full h-full' aria-hidden="true" />)}
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} className='fixed z-30 inset-0 flex items-center justify-center'>
         <Dialog.Panel className='bg-light-green p-4 rounded-2xl flex flex-col items-center '>
@@ -26,8 +27,8 @@ export default function VerificationDialog({ onAccept }: { onAccept: () => void 
             Are you sure you want to clear the board?
           </Dialog.Description>
           <div>
-            <button onClick={() => handleSelection(true)} className='bg-dark-green text-white py-1 px-3 font-bold rounded-md mr-3'>Yes</button>
-            <button onClick={() => handleSelection(false)} className='bg-accent-gray text-white py-1 px-3 font-bold rounded-md'>No</button>
+            <Button onPress={() => handleSelection(true)} className='bg-dark-green text-white py-1 px-3 font-bold rounded-md mr-3'>Yes</Button>
+            <Button onPress={() => handleSelection(false)} className='bg-accent-gray text-white py-1 px-3 font-bold rounded-md'>No</Button>
           </div>
         </Dialog.Panel>
       </Dialog>
