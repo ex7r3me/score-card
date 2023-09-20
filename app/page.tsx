215d19fdd2cd361392b69fde27c7fd2752e8d6d4
Anders Silfverberg <anders.silfverberg@regent.se>
2023-09-20 09:42:32 +0200

    Restyled the bubble to look a little better

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,20 @@ export default function Home() {
       container 
       relative
       bg-light-green 
-      rounded-2xl 
+      rounded-tl-lg
+      rounded-tr-2xl
+      rounded-br-2xl
+      rounded-bl-2xl 
       text-center 
-      m-4 
+      m-4
+      ml-0 
       p-4 
       w-4/5 
       sm:w-2/5
       before:content-[''] 
       before:block 
       before:absolute
+      before:left-2
       before:-top-2
       before:w-4
       before:h-4
