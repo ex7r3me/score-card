3ac4bf43b6c16db7c82e8e0ce0ea109359e94b34
Sia Ahmadpour <siaahmadpour@Linuss-MacBook-Pro.local>
2023-09-27 15:40:04 +0200

    update function names

--- components/yahtzee-scorecard/yahtzee-scorecard.tsx
+++ components/yahtzee-scorecard/yahtzee-scorecard.tsx
@@ -13,15 +13,15 @@ export default function YahtzeeScorecard() {
 
   const [state, dispatch] = useReducer(scorecardReducer, initialState);
 
-  const clearBoard = () => {
+  const onClearBoard = () => {
     dispatch({ type: RESET_BOARD, payload: playersCount })
   };
 
-  const handleScoreChange = (rowIndex: number, colIndex: number, value: string, scoresType: string) => {
+  const onScoreChange = (rowIndex: number, colIndex: number, value: string, scoresType: string) => {
     dispatch({ type: UPDATE_SCORE, payload: { rowIndex, colIndex, value, scoresType } });
   };
 
-  const handlePlayerChange = (index: number, value: string) => {
+  const onPlayerChange = (index: number, value: string) => {
     dispatch({ type: UPDATE_PLAYER_NAMES, payload: { index, playerName: value } });
   };
 
@@ -35,13 +35,13 @@ export default function YahtzeeScorecard() {
       <YahtzeeTableLayout
         playersCount={playersCount}
         state={state}
-        handlePlayerChange={handlePlayerChange}
-        handleScoreChange={handleScoreChange} />
+        handlePlayerChange={onPlayerChange}
+        handleScoreChange={onScoreChange} />
       <div className="flex flex-row justify-items-start items-start justify-center p-2">
         <Button onPress={() => { }} className="inline-block bg-dark-green text-white py-1 px-3 font-bold rounded-md mr-3">
           Reveal Score
         </Button>
-        <ClearButton onAccept={clearBoard} />
+        <ClearButton onAccept={onClearBoard} />
       </div>
     </div>
   );
