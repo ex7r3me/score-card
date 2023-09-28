81ac30694a8578fa79f8d384a06a528e5587e7c6
Sia Ahmadpour <siaahmadpour@Linuss-MacBook-Pro.local>
2023-09-28 09:06:27 +0200

    remove ssr from yahtzee component

--- app/yahtzee/page.tsx
+++ app/yahtzee/page.tsx
@@ -1,9 +1,11 @@
-import YahtzeeScorecard from "@/components/yahtzee-scorecard/yahtzee-scorecard";
+import dynamic from 'next/dynamic'
+const NoSSR = dynamic(() => import('@/components/yahtzee-scorecard/yahtzee-scorecard'), { ssr: false })
+
 export default function Yahtzee() {
   return (
     <div className="max-w-md p-4 m-4 ml-auto mr-auto overflow-x-auto shadow-md bg-beige rounded-2xl">
       <h1 role='heading' className="my-2 text-3xl font-bold text-center">Yahtzee</h1>
-      <YahtzeeScorecard />
+      <NoSSR />
     </div>
   );
 }
